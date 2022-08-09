5ce0c8044187ee870745e02ff552ce47fa5271cb
rodpedrassi <rodpedrassi@gmail.com>
2022-08-09 15:34:18 -0300

    req09 feito

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -23,18 +23,40 @@ class ProductDetails extends Component {
     this.setState({ product: data });
   }
 
+  addToCart = async (id, title, thumbnail, price) => {
+    const cartItem = { id, title, thumbnail, price, quantity: 1 };
+    const actualCart = JSON.parse(localStorage.getItem('cart'));
+    let newCart = [];
+    if (actualCart) {
+      newCart = [cartItem, ...actualCart];
+      if (actualCart.some((item) => item.id === cartItem.id)) {
+        newCart = actualCart;
+        newCart.forEach((item, i) => {
+          if (item.id === cartItem.id) newCart[i].quantity += 1;
+        });
+      }
+      localStorage.setItem('cart', JSON.stringify(newCart));
+    } else localStorage.setItem('cart', JSON.stringify([cartItem]));
+  };
+
   render() {
-    const { product } = this.state;
-    console.log(product);
+    const { product: { thumbnail, title, price, id } } = this.state;
     return (
       <div data-testid="product-detail-link">
         <img
-          src={ product.thumbnail }
-          alt={ product.title }
+          src={ thumbnail }
+          alt={ title }
           data-testid="product-detail-image"
         />
-        <p data-testid="product-detail-name">{ product.title }</p>
-        <p data-testid="product-detail-price">{ product.price }</p>
+        <p data-testid="product-detail-name">{ title }</p>
+        <p data-testid="product-detail-price">{ price }</p>
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ () => this.addToCart(id, title, thumbnail, price) }
+        >
+          + carrinho
+        </button>
         <Link to="/shopping-cart">
           <button type="button" data-testid="shopping-cart-button">carrinho</button>
         </Link>
