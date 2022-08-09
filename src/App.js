eab2ab5d66ce200a9b7fafd797a1c3eed3b42410
Lucas Ximenes <lucasdximenes@gmail.com>
2022-08-09 19:32:55 -0300

    G18 - Requirement 12 complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import ProductDetails from './pages/ProductDetails';
+import Checkout from './pages/Checkout';
 
 export default class App extends Component {
   render() {
@@ -12,6 +13,7 @@ export default class App extends Component {
           <Route exact path="/" component={ Home } />
           <Route exact path="/shopping-cart" component={ ShoppingCart } />
           <Route exact path="/product-details/:id" component={ ProductDetails } />
+          <Route exact path="/checkout" component={ Checkout } />
         </Switch>
       </BrowserRouter>
     );
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class ShoppingCart extends Component {
   state = {
     cart: [],
-  }
+  };
 
   componentDidMount() {
     const shoppingCart = JSON.parse(localStorage.getItem('cart'));
@@ -30,64 +30,67 @@ export default class ShoppingCart extends Component {
     }
     this.setState({ cart });
     localStorage.setItem('cart', JSON.stringify(cart));
+  };
+
+  buildCart = () => {
+    const { cart } = this.state;
+    if (cart && cart.length > 0) {
+      return cart.map((actual) => {
+        const { id, title, thumbnail, price, quantity } = actual;
+        return (
+          <div key={ id }>
+            <h3 data-testid="shopping-cart-product-name">{title}</h3>
+            <button
+              onClick={ () => this.handleRemoveItem(id) }
+              data-testid="remove-product"
+              type="button"
+            >
+              X
+            </button>
+            <img src={ thumbnail } alt={ id } />
+            <p>{price}</p>
+            <button
+              name="decrement"
+              onClick={ (event) => this.handleQuantity(event, id) }
+              data-testid="product-decrease-quantity"
+              type="button"
+            >
+              -
+            </button>
+            <span data-testid="shopping-cart-product-quantity">{quantity}</span>
+            <button
+              name="increment"
+              onClick={ (event) => this.handleQuantity(event, id) }
+              data-testid="product-increase-quantity"
+              type="button"
+            >
+              +
+            </button>
+          </div>
+        );
+      });
+    }
+    return (
+      <span data-testid="shopping-cart-empty-message">
+        Seu carrinho está vazio
+      </span>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <Link to="/">
+          <button type="button">Home</button>
+        </Link>
+        <Link to="/checkout">
+          <button type="button" data-testid="checkout-products">
+            Checkout
+          </button>
+        </Link>
+        <h1>Carrinho de Compras</h1>
+        <div>{this.buildCart()}</div>
+      </div>
+    );
   }
-
-   buildCart = () => {
-     const { cart } = this.state;
-     if (cart && cart.length > 0) {
-       return cart.map((actual) => {
-         const { id, title, thumbnail, price, quantity } = actual;
-         return (
-           <div key={ id }>
-             <h3 data-testid="shopping-cart-product-name">{title}</h3>
-             <button
-               onClick={ () => this.handleRemoveItem(id) }
-               data-testid="remove-product"
-               type="button"
-             >
-               X
-             </button>
-             <img src={ thumbnail } alt={ id } />
-             <p>{price}</p>
-             <button
-               name="decrement"
-               onClick={ (event) => this.handleQuantity(event, id) }
-               data-testid="product-decrease-quantity"
-               type="button"
-             >
-               -
-             </button>
-             <span data-testid="shopping-cart-product-quantity">{quantity}</span>
-             <button
-               name="increment"
-               onClick={ (event) => this.handleQuantity(event, id) }
-               data-testid="product-increase-quantity"
-               type="button"
-             >
-               +
-             </button>
-           </div>
-         );
-       });
-     }
-     return (
-       <span data-testid="shopping-cart-empty-message">
-         Seu carrinho está vazio
-       </span>
-     );
-   }
-
-   render() {
-     return (
-       <div>
-         <Link to="/">
-           <button type="button">Home</button>
-         </Link>
-         <h1>Carrinho de Compras</h1>
-         <div>
-           { this.buildCart() }
-         </div>
-       </div>
-     );
-   }
 }
